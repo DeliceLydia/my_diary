6cd41fb47c438c13b2f42719680df77e2e7e2a7e
delicelydia <Delydia84@gmail.com>
2022-04-06 11:21:20 +0200

    Add user authentications

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,4 +1,16 @@
 class AuthenticationsController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
   def create
+    user = User.find_by(email: params[:email])
+    token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    render json: { email: user.email, authToken: token }
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password)
   end
 end
